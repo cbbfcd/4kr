dfa9433ce057304a6dc6fe13997beda25f7fb580
huangteng01 <huangteng01@baidu.com>
2019-09-04 00:42:48 +0800

    anu createElement

---
+++ anu/core/createContext.js
@@ -0,0 +1,7 @@
+import { miniCreateClass, isFn, get } from './utils';
+import { Component } from './Component';
+import { Renderer } from './createRenderer';
+
+// 数组的最大容量
+const MAX_NUMBER = 1073741823;
+
--- anu/core/createPortal.js
+++ anu/core/createPortal.js
@@ -1,1 +1,13 @@
-// TODO: 
+import { createElement } from "./createElement";
+
+// https://zh-hans.reactjs.org/docs/portals.html
+export function AnuPortal(props) {
+    return props.children;
+}
+
+// 这里只是做了一层包装，返回了挂了特点属性的虚拟 DOM 对象，所以真正让 children 穿越的能力应该是在 ReactDom 中完成的
+export function createPortal(children, parent) {
+    let child = createElement(AnuPortal, { children, parent });
+    child.isPortal = true;
+    return child;
+}
